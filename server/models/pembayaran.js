1e68ab917fcc26b34b0f6321738be1648aef9cf0
ayalarifki <ayalarifki@gmail.com>
2024-07-04 12:29:21 +0700

    Update pembayaran.js

--- server/models/pembayaran.js
+++ server/models/pembayaran.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const pembayaranSchema = new Schema({
-  id_pengguna: { type: Schema.Types.ObjectId, ref: 'users', required: true },
+  id_pengguna: { type: Schema.Types.ObjectId, ref: 'Users', required: true },
   metode_pembayaran: { type: String, required: true },
   bukti_pembayaran: { type: String }, // Menggunakan sub-skema untuk bukti_pembayaran
-  status_pembayaran: { type: String, required: true, default: "Pending" }
+  status_pembayaran: { type: String, required: true, default: "Pending" },
+  tanggal_pembayaran: { type: Date, required: true}
 });
 
 const collectionName = 'Pembayaran';
